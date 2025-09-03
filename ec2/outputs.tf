c6f367ddfac3973fe50c58ffeab1974de9ec596f
EC2 Default User <ec2-user@ip-172-31-41-146.ap-south-1.compute.internal>
2025-09-03 07:04:34 +0000

    updates terraform artifcats

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -9,3 +9,15 @@ resource "local_file" "example_ips" {
   filename = "${path.module}/ec2_ips.txt"
   
 }
+
+# creating ansible inventory like file 
+resource "null_resource" "stop_ipaddress" {
+  depends_on = [ aws_instance.example ]
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[hello]" >ip.txt
+      echo "${join("\n",aws_instance.example[*].public_ip)}" >>ip.txt
+      EOT   
+  }
+  
+}
